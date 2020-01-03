b089e218773fc8a2ad7858eafbeafe2c63014674
Kameron Tanseli <kamerontanseliwork@gmail.com>
2020-01-03 19:53:09 +0000

    first commit

--- src/background.js
+++ src/background.js
@@ -1,21 +1,112 @@
-'use strict';
+"use strict";
 
-// With background scripts you can communicate with popup
-// and contentScript files.
-// For more information on background script,
-// See https://developer.chrome.com/extensions/background_pages
+function getDayMonthYear(date = new Date()) {
+  return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+}
 
-chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.type === 'GREETINGS') {
-    const message = `Hi ${
-      sender.tab ? 'Con' : 'Pop'
-    }, my name is Bac. I am from Background. It's great to hear from you.`;
+function Notifications() {
+  var alarmName = "remindme";
 
-    // Log message coming from the `request` parameter
-    console.log(request.payload.message);
-    // Send a response message
-    sendResponse({
-      message,
+  function checkAlarm(callback) {
+    console.log("check alarms");
+    chrome.alarms.getAll(function(alarms) {
+      var hasAlarm = alarms.some(function(a) {
+        return a.name == alarmName;
+      });
+      callback(hasAlarm);
     });
+  }
+
+  function setAlarm() {
+    chrome.storage.sync.get(["preferences"], function(result) {
+      let preferences = result.preferences || { litres: 2.2, reminders: 30 };
+      console.log(`Alarm set every ${preferences.reminders} minutes`);
+      chrome.alarms.create(alarmName, {
+        delayInMinutes: preferences.reminders,
+        periodInMinutes: preferences.reminders
+      });
+    });
+  }
+
+  function cancelAlarm() {
+    chrome.alarms.clear(alarmName);
+  }
+
+  function toggleAlarm() {
+    checkAlarm(function(hasAlarm) {
+      if (hasAlarm) {
+        cancelAlarm();
+        setAlarm();
+      } else {
+        setAlarm();
+      }
+    });
+  }
+
+  return {
+    toggle: toggleAlarm
+  };
+}
+
+function showDrinkNotification() {
+  chrome.notifications.create(
+    undefined,
+    {
+      type: "basic",
+      iconUrl: "icons/icon_144.png",
+      title: "Drink Water!",
+      message: "Drink a glass of water to stay hydrated and focused",
+      buttons: [
+        {
+          title: "Skip"
+        },
+        {
+          title: "Drink"
+        }
+      ]
+    },
+    function() {}
+  );
+}
+
+function addCup(cb) {
+  chrome.storage.sync.get(["timeline"], function(result) {
+    const today = getDayMonthYear();
+    const timeline = result.timeline || {};
+    const drinksToday = timeline[today] || [];
+    drinksToday.push(240);
+    chrome.storage.sync.set(
+      {
+        timeline: {
+          ...timeline,
+          [today]: drinksToday
+        }
+      },
+      function() {
+        console.log("Drunk!");
+        cb();
+      }
+    );
+  });
+}
+
+Notifications().toggle();
+
+chrome.runtime.onMessage.addListener(function(request) {
+  if (request.type === "state") {
+    Notifications().toggle();
+  }
+});
+
+chrome.alarms.onAlarm.addListener(showDrinkNotification);
+
+chrome.notifications.onButtonClicked.addListener(function(id, btnIndex) {
+  if (btnIndex === 1) {
+    addCup(function() {
+      chrome.notifications.clear(id);
+      chrome.runtime.sendMessage({ type: "drunk" });
+    });
+  } else {
+    chrome.notifications.clear(id);
   }
 });
--- src/popup.js
+++ src/popup.js
@@ -1,6 +1,6 @@
-'use strict';
+"use strict";
 
-import './popup.css';
+import "./popup.css";
 
 (function() {
   // We will make use of Storage API to get and store `count` value
@@ -10,103 +10,92 @@ import './popup.css';
   // To get storage access, we have to mention it in `permissions` property of manifest.json file
   // More information on Permissions can we found at
   // https://developer.chrome.com/extensions/declare_permissions
-  const counterStorage = {
-    get: cb => {
-      chrome.storage.sync.get(['count'], result => {
-        cb(result.count);
-      });
-    },
-    set: (value, cb) => {
+
+  function PreferencesForm() {
+    var form = document.querySelector("form");
+    const button = form.querySelector("button");
+    const litres = form.querySelector('[name="litres"]');
+    const reminders = form.querySelector('[name="reminders"]');
+
+    chrome.storage.sync.get(["preferences"], function(result) {
+      let preferences = result.preferences || { litres: 2.2, reminders: 30 };
+      litres.value = preferences.litres;
+      reminders.value = preferences.reminders;
+    });
+
+    function animateBtnSaving() {
+      button.innerText = "Saving...";
+      button.disabled = true;
+
+      setTimeout(() => {
+        button.innerText = "Save";
+        button.disabled = false;
+      }, 1000);
+    }
+
+    function saveValues(cb) {
       chrome.storage.sync.set(
         {
-          count: value,
+          preferences: {
+            litres: Number(litres.value),
+            reminders: Number(reminders.value)
+          }
         },
-        () => {
-          cb();
-        }
+        cb
       );
-    },
-  };
-
-  function setupCounter(initialValue = 0) {
-    document.getElementById('counter').innerHTML = initialValue;
-
-    document.getElementById('incrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'INCREMENT',
-      });
-    });
-
-    document.getElementById('decrementBtn').addEventListener('click', () => {
-      updateCounter({
-        type: 'DECREMENT',
-      });
-    });
-  }
-
-  function updateCounter({ type }) {
-    counterStorage.get(count => {
-      let newCount;
-
-      if (type === 'INCREMENT') {
-        newCount = count + 1;
-      } else if (type === 'DECREMENT') {
-        newCount = count - 1;
-      } else {
-        newCount = count;
-      }
-
-      counterStorage.set(newCount, () => {
-        document.getElementById('counter').innerHTML = newCount;
-
-        // Communicate with content script of
-        // active tab by sending a message
-        chrome.tabs.query({ active: true, currentWindow: true }, tabs => {
-          const tab = tabs[0];
-
-          chrome.tabs.sendMessage(
-            tab.id,
-            {
-              type: 'COUNT',
-              payload: {
-                count: newCount,
-              },
-            },
-            response => {
-              console.log('Current count value passed to contentScript file');
-            }
-          );
-        });
-      });
-    });
-  }
-
-  function restoreCounter() {
-    // Restore count value
-    counterStorage.get(count => {
-      if (typeof count === 'undefined') {
-        // Set counter value as 0
-        counterStorage.set(0, () => {
-          setupCounter(0);
-        });
-      } else {
-        setupCounter(count);
-      }
-    });
-  }
-
-  document.addEventListener('DOMContentLoaded', restoreCounter);
-
-  // Communicate with background file by sending a message
-  chrome.runtime.sendMessage(
-    {
-      type: 'GREETINGS',
-      payload: {
-        message: 'Hello, my name is Pop. I am from Popup.',
-      },
-    },
-    response => {
-      console.log(response.message);
     }
-  );
+
+    form.addEventListener("submit", function(e) {
+      e.preventDefault();
+      saveValues(() => {
+        animateBtnSaving();
+        chrome.runtime.sendMessage({ type: "state" });
+      });
+    });
+  }
+
+  function Water() {
+    function getDayMonthYear(date = new Date()) {
+      return `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}`;
+    }
+
+    function render() {
+      chrome.storage.sync.get(["timeline", "preferences"], function(result) {
+        let preferences = result.preferences || { litres: 2.2, reminders: 30 };
+        const today = getDayMonthYear();
+        const timeline =
+          result.timeline && Array.isArray(result.timeline[today])
+            ? result.timeline[today]
+            : [];
+        const percent =
+          (timeline.reduce((total, next) => total + next, 0) /
+            1000 /
+            preferences.litres) *
+          100;
+
+        document.querySelector(".glass__total").innerText = `${timeline.reduce(
+          (total, next) => total + next,
+          0
+        ) / 1000} L`;
+
+        document.querySelector(".glass__water").style.height = `${Math.max(
+          15,
+          100 - percent
+        )}%`;
+      });
+    }
+
+    chrome.runtime.onMessage.addListener(function(request) {
+      if (request.type === "water") {
+        render();
+      }
+    });
+
+    render();
+  }
+
+  document.addEventListener("DOMContentLoaded", function() {
+    PreferencesForm();
+    Water();
+  });
 })();
